ab6b472c740e349c382618ccc740934daf0ea048
tobiasfrick <hans.tobias.frick@gmail.com>
2020-06-08 17:14:21 +0200

    User-defined output name

--- src/blr/cli/naibrconfig.py
+++ src/blr/cli/naibrconfig.py
@@ -20,18 +20,19 @@ def add_arguments(parser):
     parser.add_argument("bamfile", type=Path, help="Config: Bam file for which LSVs should be called.")
     parser.add_argument("naibr_out", type=Path, help="Config: NAIBR output folder.")
     parser.add_argument("--threads", type=int, default=1, help="Config: Number of threads for which to run NAIBR.")
+    parser.add_argument("-o", "--output", type=Path, required=True, help="Name of output config file.")
 
 
 def main(args):
-    new_value = {"bam_file": str(args.bamfile), "outdir": str(args.naibr_out),
+    new_value = {"bam_file": str(args.bamfile.resolve()), "outdir": str(args.naibr_out.resolve()),
                  "threads": str(args.threads)}
     mod_keys = tuple(new_value.keys())
-    copy_and_mod_config(args.analysis_folder, CONFIGURATION_FILE_NAME, new_value, mod_keys)
+    copy_and_mod_config(args.analysis_folder, CONFIGURATION_FILE_NAME, args.output, new_value, mod_keys)
 
 
-def copy_and_mod_config(analysis_folder: Path, file_name: Path, new_value: dict, mod_keys: tuple):
-    configuration = open_text("blr", file_name)
-    with (analysis_folder / file_name).open("w") as f:
+def copy_and_mod_config(analysis_folder: Path, template_file: Path, output_file: Path, new_value: dict, mod_keys: tuple):
+    configuration = open_text("blr", template_file)
+    with (analysis_folder / output_file).open("w") as f:
         for row in configuration:
             if row.startswith(mod_keys):
                 row = change_row(row, new_value)
