a51e150daf599f2eabb48a63ad91eb40d9d4f071
Pontus Hojer <pontushojer@gmail.com>
2020-07-31 12:36:59 +0200

    Add heapq sorting for ema mapping to tagfastq.py. This avoids lexicographical sorting of barcodes that cause an issue of grouping non-connected barcodes in ema mapping (see https://github.com/FrickTobias/BLR/issues/219). Here instead barcodes are grouped and sorted based on a index (effectively by barcode cluster size).

--- src/blr/cli/tagfastq.py
+++ src/blr/cli/tagfastq.py
@@ -23,6 +23,9 @@ import sys
 import dnaio
 from itertools import islice
 from collections import Counter
+import heapq
+from pathlib import Path
+import tempfile
 
 from blr.utils import tqdm, print_stats
 
@@ -30,48 +33,91 @@ logger = logging.getLogger(__name__)
 
 
 def main(args):
+    import time
+    s = time.time()
+    run_tagfastq(
+        uncorrected_barcodes=args.uncorrected_barcodes,
+        corrected_barcodes=args.corrected_barcodes,
+        input1=args.input1,
+        input2=args.input2,
+        output1=args.output1,
+        output2=args.output2,
+        barcode_tag=args.barcode_tag,
+        sequence_tag=args.sequence_tag,
+        mapper=args.mapper,
+        skip_singles=args.skip_singles
+    )
+    logger.info(f"Total runtime: {time.time() - s} s")
+
+
+def run_tagfastq(
+        uncorrected_barcodes: str,
+        corrected_barcodes: str,
+        input1: str,
+        input2: str,
+        output1: str,
+        output2: str,
+        barcode_tag: str,
+        sequence_tag: str,
+        mapper: str,
+        skip_singles: bool
+):
     logger.info("Starting")
     summary = Counter()
     # Get the corrected barcodes and create a dictionary pointing each raw barcode to its
     # canonical sequence.
-    with open(args.corrected_barcodes, "r") as reader:
-        corrected_barcodes = parse_corrected_barcodes(reader, summary, skip_singles=args.skip_singles)
+    with open(corrected_barcodes, "r") as reader:
+        corrected_barcodes, heap = parse_corrected_barcodes(reader, summary, skip_singles=skip_singles)
 
-    in_interleaved = not args.input2
+    in_interleaved = not input2
     logger.info(f"Input detected as {'interleaved' if in_interleaved else 'paired'} FASTQ.")
 
     # If no output1 is given output is sent to stdout
-    if not args.output1:
+    if not output1:
         logger.info("Writing output to stdout.")
-        args.output1 = sys.stdout.buffer
-        args.output2 = None
+        output1 = sys.stdout.buffer
+        output2 = None
 
-    out_interleaved = not args.output2
+    out_interleaved = not output2
     logger.info(f"Output detected as {'interleaved' if out_interleaved else 'paired'} FASTQ.")
 
-    # Parse input FASTA/FASTQ for read1 and read2, uncorrected barcodes and write output
-    with dnaio.open(args.input1, file2=args.input2, interleaved=in_interleaved, mode="r",
-                    fileformat="fastq") as reader, \
-            dnaio.open(args.output1, file2=args.output2, interleaved=out_interleaved, mode="w",
-                       fileformat="fastq") as writer, \
-            BarcodeReader(args.uncorrected_barcodes) as uncorrected_barcode_reader:
+    # Required for ema sorted output
+    # Inspired by: https://stackoverflow.com/questions/56948292/python-sort-a-large-list-that-doesnt-fit-in-memory
+    output_chunk = list()
+    chunk_size = 1_000_000  # int(summary["Reads with corrected barcodes"] / 100)  # Use 100 bins
+    logger.info(f"Using chunks of size {chunk_size} for sorting.")
+    chunk_id = 0
+    tmpdir = Path(tempfile.mkdtemp(prefix="tagfastq_sort"))
+    chunk_file_template = "chunk_*.tsv"
+    chunk_sep = "\t"
 
-        for read1, read2 in tqdm(reader, desc="Read pairs processed"):
+    # Parse input FASTA/FASTQ for read1 and read2, uncorrected barcodes and write output
+    with dnaio.open(input1, file2=input2, interleaved=in_interleaved, mode="r",
+                    fileformat="fastq") as reader, \
+            dnaio.open(output1, file2=output2, interleaved=out_interleaved, mode="w",
+                       fileformat="fastq") as writer, \
+            BarcodeReader(uncorrected_barcodes) as uncorrected_barcode_reader:
+        if mapper == "ema":
+            tmp_writer = open(tmpdir / chunk_file_template.replace("*", str(chunk_id)), "w")
+
+        for read1, read2 in tqdm(reader, desc="Read pairs processed", disable=False):
             # Header parsing
+            summary["Read pairs read"] += 1
             name_and_pos, nr_and_index1 = read1.name.split(maxsplit=1)
             _, nr_and_index2 = read2.name.split(maxsplit=1)
 
             uncorrected_barcode_seq = uncorrected_barcode_reader.get_barcode(name_and_pos)
+            corrected_barcode_seq = None
 
             # Check if barcode was found and update header with barcode info.
             if uncorrected_barcode_seq and uncorrected_barcode_seq in corrected_barcodes:
                 corrected_barcode_seq = corrected_barcodes[uncorrected_barcode_seq]
 
-                raw_barcode_id = f"{args.sequence_tag}:Z:{uncorrected_barcode_seq}"
-                corr_barcode_id = f"{args.barcode_tag}:Z:{corrected_barcode_seq}"
+                raw_barcode_id = f"{sequence_tag}:Z:{uncorrected_barcode_seq}"
+                corr_barcode_id = f"{barcode_tag}:Z:{corrected_barcode_seq}"
 
                 # Create new name with barcode information.
-                if args.mapper == "ema":
+                if mapper == "ema":
                     # The EMA aligner requires reads in 10x format e.g.
                     # @READNAME:AAAAAAAATATCTACGCTCA BX:Z:AAAAAAAATATCTACGCTCA
                     new_name = ":".join([name_and_pos, corrected_barcode_seq])
@@ -85,13 +131,64 @@ def main(args):
                 summary["Reads missing barcode"] += 1
 
                 # EMA aligner cannot handle reads without barcodes so these are skipped.
-                if args.mapper == "ema":
+                if mapper == "ema":
                     continue
 
             # Write to out
-            writer.write(read1, read2)
+            if mapper == "ema":
+                output_chunk.append(
+                    chunk_sep.join((
+                        str(heap[corrected_barcode_seq]),
+                        read1.name,
+                        read1.sequence,
+                        read1.qualities,
+                        read2.name,
+                        read2.sequence,
+                        read2.qualities,
+                        "\n"
+                    ))
+                )
+                if not summary["Read pairs read"] % chunk_size:
+                    output_chunk = sorted(output_chunk, key=lambda x: int(x.split(chunk_sep)[0]))
+                    tmp_writer.writelines(output_chunk)
+                    output_chunk.clear()
+                    tmp_writer.close()
+                    chunk_id += 1
+                    tmp_writer = open(tmpdir / chunk_file_template.replace("*", str(chunk_id)), "w")
 
-    print_stats(summary, name=__name__)
+            else:
+                summary["Read pairs written"] += 1
+                writer.write(read1, read2)
+
+        # Empty final cache
+        if mapper == "ema":
+            if output_chunk:
+                logger.info(f"Writing chunk {chunk_id}, final")
+                output_chunk = sorted(output_chunk, key=lambda x: int(x.split(chunk_sep)[0]))
+                tmp_writer.writelines(output_chunk)
+
+            tmp_writer.close()
+
+            logger.info(f"Opening chunks for merge")
+            chunks = []
+            for chunk in tmpdir.iterdir():
+                chunks.append(open(chunk))
+
+            logger.info(f"Merging chunks")
+            for entry in tqdm(heapq.merge(*chunks, key=lambda x: int(x.split(chunk_sep)[0])), desc="Merging chunks"):
+                entry = entry.split(chunk_sep)
+                r1 = dnaio.Sequence(*entry[1:4])
+                r2 = dnaio.Sequence(*entry[4:7])
+                writer.write(r1, r2)
+                summary["Read pairs written"] += 1
+
+            for open_chunk in chunks:
+                try:
+                    open_chunk.close()
+                except AttributeError:
+                    pass
+
+    print_stats(summary, __name__)
 
     logger.info("Finished")
 
@@ -106,14 +203,19 @@ def parse_corrected_barcodes(open_file, summary, skip_singles=False):
     :return: dict: raw sequences pointing to a corrected canonical sequence.
     """
     corrected_barcodes = dict()
-    for cluster in tqdm(open_file, desc="Clusters processed"):
-        summary["Clusters"] += 1
+    heap_index = dict()
+    for index, cluster in tqdm(enumerate(open_file), desc="Clusters processed"):
+        summary["Corrected barcodes"] += 1
         canonical_seq, size, cluster_seqs = cluster.strip().split("\t", maxsplit=3)
         if skip_singles and int(size) == 1:
             summary["Clusters size 1 skipped"] += 1
             continue
+        summary["Reads with corrected barcodes"] += int(size)
+        summary["Uncorrected barcodes"] += len(cluster_seqs.split(","))
         corrected_barcodes.update({raw_seq: canonical_seq for raw_seq in cluster_seqs.split(",")})
-    return corrected_barcodes
+
+        heap_index[canonical_seq] = index
+    return corrected_barcodes, heap_index
 
 
 class BarcodeReader:
@@ -123,7 +225,7 @@ class BarcodeReader:
         self.barcodes = self.parse()
 
     def parse(self):
-        for barcode in tqdm(self._file, desc="Uncorrected barcodes processed"):
+        for barcode in tqdm(self._file, desc="Uncorrected barcodes processed", disable=True):
             read_name, _ = barcode.name.split(maxsplit=1)
             yield (read_name, barcode.sequence)
 
