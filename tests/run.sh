4fde8acb354913cff8fb7c12d91b853a6fffeb03
Pontus Hojer <pontushojer@gmail.com>
2020-08-12 15:57:42 +0200

    Update md5sum

--- tests/run.sh
+++ tests/run.sh
@@ -44,4 +44,4 @@ blr config \
 cd outdir-bowtie2
 blr run
 m=$(samtools view final.bam | $md5 | cut -f1 -d" ")
-test $m == 7347a7c126eb5ef3b758410afebbc1ff
+test $m == 5314f2f26d094fb1dc0b640f893918b7
