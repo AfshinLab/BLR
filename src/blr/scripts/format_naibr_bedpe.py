9dcbc4326f195db7461aa03aaf787838b4c0ff4d
Pontus Hojer <pontushojer@gmail.com>
2021-01-20 12:21:45 +0100

    Change from 10x BEDPE format to LinkedSV for coming LinkedSV integration.

--- src/blr/scripts/format_naibr_bedpe.py
+++ src/blr/scripts/format_naibr_bedpe.py
@@ -12,23 +12,25 @@ names = ["Chr1", "Break1", "Chr2", "Break2", "SplitMolecules", "DiscordantReads"
          "Haplotype", "Score", "PassFilter"]
 data = pd.read_csv(snakemake.input.tsv, sep="\t", header=0, names=names)
 with open(snakemake.output.bedpe, "w") as file:
+    # Header
+    print("#chrom1", "start1", "stop1", "chrom2", "start2", "stop2", "sv_type", "sv_id", "sv_length", "qual_score",
+          "filter", "info", sep="\t", file=file)
+
     for nr, row in enumerate(data.itertuples(index=False)):
-        # Use BEDPE format according to 10x: https://support.10xgenomics.com/genome-exome/software/pipelines/latest/output/bedpe
-        # Supported by IGV (see https://github.com/igvteam/igv/wiki/BedPE-Support)
+        # Use BEDPE format according to LinkedSV: https://github.com/WGLab/LinkedSV#-sv-call-file
         print(
             f"chr{row.Chr1}",
             row.Break1,
-            row.Break1,
+            row.Break1+1,
             f"chr{row.Chr2}",
             row.Break2,
-            row.Break2,
-            f"call_{nr}",
+            row.Break2+1,
+            sv_types[row.Orientation],
+            f"ID{nr:04}",
+            abs(row.Break1 - row.Break2),
             row.Score,
-            "+",
-            "+",
-            "." if row.PassFilter == "PASS" else "Filtered",
-            "Type={};Zygosity={};Split_molecules={};Discordant_reads={};Orientation={};Haplotype={}".format(
-                sv_types[row.Orientation],
+            row.PassFilter,
+            "ZYGOSITY={};NUM_SPLIT_MOLECULES={};NUM_DISCORDANT_READS={};ORIENTATION={};HAPLOTYPE={}".format(
                 zygosity.get(row.Haplotype, "UNK"),
                 row.SplitMolecules,
                 row.DiscordantReads,
