ca276980a8e7847dcfdeb01fc167441f536f6f8b
Humam Siga <46369083+HSiga@users.noreply.github.com>
2021-01-26 16:07:30 +0100

    Update reformat_naibr_output.R
    Add [type, lengths, quantile filter, distance] options.
    distance option is used for merging the collapsed regions
    remove black-lists
    Generate bedpe for plotting in IGV

--- SV_analysis/reformat_naibr_output.R
+++ SV_analysis/reformat_naibr_output.R
@@ -1,115 +1,107 @@
-library("optparse")
+###
+# This code is to filter bedpe file generated from Naibr and make it
+# compatible with SURVIVOR, with additional filtration of lengths
+# and quality. 
+###
 
-system("defaults write org.R-project.R force.LANG en_US.UTF-8")
+library(optparse)
+
+system("export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8")
 
 option_list = list(
-  make_option(c("-f", "--file"), type="character", default="file", 
-              help="dataset file name", metavar="character"),
-  make_option(c("-o", "--type"), type="character", default="DEL", 
-              help="Choose which SV type to use [DEL, DUP, INV] [default= %default]", metavar="character"),
-  make_option(c("-o", "--out"), type="character", default="out.txt", 
-              help="output file name [default= %default]", metavar="character")
+  make_option(c("-f", "--file"), type="character", default="", 
+              help="bedpe file generated from NAIBR", metavar="character"),
+  make_option(c("-t", "--type"), type="character", default="DEL", 
+              help="Choose which SV type to keep [DEL, DUP, INV] [default= %default]", metavar="character"),
+  make_option(c("-M", "--maxlength"), type="integer", default=10000000, 
+              help="Max SV length to keep [default= %default]", metavar="integer"),
+  make_option(c("-m", "--minlength"), type="integer", default=10000, 
+              help="Min SV length to keep [default= %default]", metavar="integer"),
+  make_option(c("-q", "--quantile"), type="double", default=0.1, 
+              help="Remove SV with QC less than the quantile [default= %default]", metavar="double"),
+  make_option(c("-d", "--max_distance"), type="integer", default=10000, 
+              help="Max distance between SV to be merged [default= %default]", metavar="integer")
 ); 
 
 
 ##################
-# 
-# opt_parser = OptionParser(option_list=option_list);
-# opt = parse_args(opt_parser);
-# 
-# df <- read.table(opt$file)
-# type <- opt$type
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
 
-## for testing ##
-df <- read.table('201026.BLR.M10_merge.ema_final.naibr_sv_calls.bedpe')
-type <- 'DEL'
+if (nchar(opt$file)>0) {
+  df <- read.table(opt$file)
+  file_name <- opt$file
+  file_name <- gsub("\\.[[:alnum:]]+$", "\\1", file_name) 
+} else {
+  stop("Date parameter must be provided. See script usage (--help)")
+}
+
+type <- opt$type
+maxlength <- opt$maxlength
+minlength <- opt$minlength
+quant <- opt$quantile
+max_distance <- opt$max_distance
+
+# ## for testing ##
+ # df <- read.table('201026.BLR.M10_merge.ema_final.naibr_sv_calls.bedpe')
+ # type <- 'DEL'
+ # file_name <- 'test'
 
 
 naibr_to_linkedSV <- function(df) {
-  
   type <- gsub(pattern = "Type=(.*);Zygosity.*", "\\1", x = df$V12)
-  
   col12 <- gsub(pattern = "Type=*(.*?)\\s*;", replacement = "", x = df$V12)
-  
   df <- data.frame(df[,1:6],type,df[,7], (df$V6-df$V2), df[,8],"PASS",col12)
-  
   colnames(df) <- c("#chrom1",	"start1",	"stop1",	"chrom2",	"start2",	"stop2",	"sv_type",
                     "sv_id",	"sv_length",	"qual_score",	"filter",	"info")
-  
   return(df)
 }
 
-df <- naibr_to_linkedSV(df)
-#df <- df[df$sv_type=="DEL",]
-
-## We need to merge SVs if:
-##  - they are of the same type (deletions duplications, and inversions)
-## AND
-##  -- start are within xx bp
-##  -- end within xx bp
-##  OR
-##  -- they overlap in more than 90 
-# 
-# library(HelloRanges)
-# 
-# testpair <- Pairs(first = df,second = df,names =)
-# findOverlapPairs(query = df,subject = df)
-
-merge_two_lines <- function(line1,line2) {
-  if (line1$sv_type==line2$sv_type && line1[1]==line2[1] && line1[4]==line2[4]) {
-    line3 <- line1
-    line3$start1 <- min(line1$start1, line2$start1)
-    line3$stop1  <- min(line1$stop1 , line2$stop1)
-    
-    line3$start2 <- max(line1$start2, line2$start2)
-    line3$stop2  <- max(line1$stop2 , line2$stop2)
-    
-  line3$sv_length <- line3$stop2  - line3$start1
-  line3$sv_id <- paste0(line1$sv_id,'_',line2$sv_id)
-  line3$info  <- paste0(line1$info,'_',line2$info)
-  return(line3)
-  } else {return(rbind(line1,line2))}
+bedpe_to_bed <- function(df) {
+  ## only for deletions, inversion and duplication 
+  df <- data.frame(df[,1:2],df[,5],df[,7:ncol(df)])
+ return(df)
+  
+}
+bed_to_bedpe <- function(df) {
+  ## For plotting in IGV, start1 = end1, start2 = end2, chr1 = chr2
+  df <- data.frame(df[,1:2],df[,2],df[,1],df[,3],df[,3],df[,4:ncol(df)])
+  return(df)
 }
 
 
+df <- naibr_to_linkedSV(df)
+df <- df[df$sv_type==type,]
 
-library(GenomicRanges)
-check_intersect <- function(line1,line2) {
- range1 <- GRanges ( paste0(line1[1],":",line1$start1,"-",line1$stop2) )
- range2 <- GRanges ( paste0(line2[1],":",line2$start1,"-",line2$stop2) )
- bins1 <- disjoin(range1)
- bins2 <- disjoin(range2)
+chrOrder<-c(paste("chr",1:22,sep=""),"chrX","chrY","chrM","chrEBV")
+df$`#chrom1` <- factor(df$`#chrom1`, levels = chrOrder) 
+df <- df[order(df$`#chrom1`, df$start1),]
+df <- bedpe_to_bed(df)
+df <- df[df$sv_length <= maxlength,]
+df <- df[df$sv_length >= minlength,]
 
- number <- countOverlaps(bins1, bins2)
- return(number)
-      #VERY SLOW
-      # x1 <- as.numeric(line1[2])
-      # x2 <- as.numeric(line1[5])
-      # x3 <- as.numeric(line2[2])
-      # x4 <- as.numeric(line2[5])
-      # a <- length( intersect(seq(x1,x2,1), seq(x3,x4,1)) )
-      # return(a)
-  }
+#summary(df)
+#hist(df$qual_score,breaks = 100,freq = F)
+#length(df$qual_score[df$qual_score<=quant10])
+quant <- quantile(df$qual_score,c(quant))
+df <- df[df$qual_score>=quant,]
 
+bedfile       <- paste0(file_name,"_filtered_sorted.bed")
+bedfile_merged <- paste0(file_name,"_filtered_sorted_merged.bed")
+bedpefile_merged   <- paste0(file_name,"_filtered_sorted_merged.bedpe")
+vcffile_merged <-  paste0(file_name,"_filtered_sorted_merged.vcf")
 
-# check_intersect(df[6,],df[4,])
-# 
-# merge_two_lines(df[6,],df[7,])
-# 
-# df <- df[1:1000,]
-# 
-# df2 <- data.frame()
-# for (row1 in 1:nrow(df)) {
-#   for (row2 in row1:nrow(df)) {
-#     line1 <- df[row1,]
-#     line2 <- df[row2,]
-#     if (line1==line2) next
-#     if(check_intersect(line1,line2))
-#        {df2 <- rbind(df2,merge_two_lines(line1,line2))}
-#   }
-# }
+write.table(df, file = bedfile ,sep = '\t', row.names = F, col.names = F, quote = F)
+system( sprintf("bedtools merge -i %s -c 4,5,6,7,8,9 -o first,collapse,max,mean,first,count -d %d > tmp_%s", bedfile,max_distance,bedfile_merged) )
 
+#removing blacklists and gaps
+system( sprintf("bedtools intersect -v -a tmp_%1$s -b hg38_black_list.bed -b hg38_gap.bed > %1$s", bedfile_merged))
 
-write.table(file = opt$out, naibr_to_linkedSV(df),
-            quote = F,row.names = F,col.names = F,sep = "\t")
+dd <- read.table(bedfile_merged)
+write.table(bed_to_bedpe(dd), file = bedpefile_merged ,sep = '\t', row.names = F, col.names = F, quote = F)
+
+system( sprintf("SURVIVOR bedpetovcf %s tmp_%s",bedpefile_merged,vcffile_merged))
+system( sprintf("less tmp_%1$s | sed s'/STRANDS=[0-9][- 0-9];//'g > %1$s",vcffile_merged))
+system( sprintf("rm %s %s tmp_%s tmp_%s",bedfile,bedfile_merged,bedfile_merged,vcffile_merged))
