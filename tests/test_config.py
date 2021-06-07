dfd3af812f768f4a703b3199a9136b27b2afcdff
Pontus Hojer <pontushojer@gmail.com>
2021-06-07 11:34:03 +0200

    Add --update-from to merge existing configuration

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-from blr.cli.config import make_paths_absolute
+from blr.cli.config import make_paths_absolute, flatten, update_changes_set
 from .utils import tempinput
 
 
@@ -23,3 +23,25 @@ def test_make_paths_absolute():
         relative_symlink_path = str(file.parent / ".." / file.parents[0] / "tmp_symlink")
         assert make_paths_absolute(relative_symlink_path) == symlink_path
         os.unlink(symlink_path)
+
+
+def test_flatten():
+    nested = {"key": {"subkey": "value"}}
+    flattened = flatten(nested)
+    assert "key.subkey" in flattened
+    assert flattened["key.subkey"] == "value"
+
+
+def test_update_changes_set():
+    configs = {
+        "foo": 3,
+        "bar": 4,
+        "nested": {"key": "new_value"}
+    }
+    changes_set = [("bar", "1"), ("nested.key", "old_value")]
+    changes_set = update_changes_set(changes_set, configs)
+
+    changes = dict(changes_set)
+    assert changes["foo"] == "3"
+    assert changes["bar"] == "1"
+    assert changes["nested.key"] == "old_value"
