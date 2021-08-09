9ff15e92c19cbb25576ed08295cc36ff55135f61
Pontus Höjer <pontushojer@gmail.com>
2021-08-09 14:12:57 +0200

    Keep parsed configs YAML for multiqc report and make enable including configs from multiple workdirs in report. Also made section collapsable.

--- src/blr/scripts/report_configs.py
+++ src/blr/scripts/report_configs.py
@@ -1,6 +1,10 @@
 """
 Parse configs from dict to YAML for MultiQC report
 """
+from string import ascii_lowercase
+from random import sample
+import os
+
 
 def parse_nested(d, root=None):
     for key, value in d.items():
@@ -10,16 +14,24 @@ def parse_nested(d, root=None):
         else:
             yield new_root, value
 
+
 with open(snakemake.output.yaml, "w") as out:
-    text = "id: 'run_configs'\n"
-    text += "section_name: 'Run configs'\n"
+    unique_id = "".join(sample(ascii_lowercase, 10))
+    workdir = os.path.dirname(os.path.abspath(snakemake.input.yaml))
+    text = "parent_id: 'configs'\n"
+    text += "parent_name: 'Configurations'\n"
+    text += f"id: 'run-configs-{unique_id}'\n"
+    text += f"section_name: 'Run configs'\n"
     text += "plot_type: 'html'\n"
-    text += "description: ' for current analysis. Describes which parameter values was used.'\n"
+    text += f"description: 'Configuration parameters used for run found in `{workdir}`.'\n"
     text += "data: |\n"
+    text += f"\t<button class='btn btn-default' data-toggle='collapse' data-target='#{unique_id}'>Show configs</button>".expandtabs(4)
+    text += f"\t<div id='{unique_id}' class='collapse'>".expandtabs(4)
     text += "\t<dl class=\"dl-horizontal\">\n".expandtabs(4)
     for param, value in parse_nested(snakemake.params.configs):
         if param.startswith("_"):
             continue
         text += f"\t\t<dt>{param}</dt><dd><samp>{value}</samp></dd>\n".expandtabs(4)
     text += "\t</dl>".expandtabs(4)
+    text += "\t</div>".expandtabs(4)
     print(text, file=out)
